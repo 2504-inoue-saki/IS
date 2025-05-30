88e6f801d410d6e5d1c6a7c66bc9921a481299a8
suzuki_shota <suzuki.shota@alhinc.jp>
2025-05-30 11:34:44 +0900

    サービスパッケージ追加

---
+++ src/main/java/com/example/IS/service/BranchService.java
@@ -0,0 +1,11 @@
+package com.example.IS.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BranchService {
+    @Autowired
+    BranchRepository branchRepository;
+
+}
