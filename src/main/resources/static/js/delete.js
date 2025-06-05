b1f188ab8433cb997e729cdb64069ce9f5a1c291
inoue_saki <inoue.saki@alhinc.jp>
2025-06-05 16:23:19 +0900

    # JS

--- src/main/resources/static/js/delete.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	$('.delete').on('click', function() {
-		if (confirm($(this).parents('.delete-button').find('input[name="deleteContent"]').val()+ "：\n" + $(this).parents('.delete-button').find('input[name="deleteInfo"]').val() + "\n削除します。よろしいですか?")) {
-			return true;
-		}
-		return false;
-	});
-});
